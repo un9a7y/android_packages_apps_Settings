7d015c9c06d0336c02bbede24b1272e18aa14c9b
Der-Schubi <schubi@erlangen.ccc.de>
2013-07-31 09:18:17 -0300

    Advanced reboot enabled by default (1/2)
    Change-Id: Ie8ab34ecc532d14d296e33fa568e6cf3ebc681e8
    
    Conflicts:
    
    	src/com/android/settings/DevelopmentSettings.java

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -257,6 +257,7 @@ public class DevelopmentSettings extends PreferenceFragment
         mPassword = (PreferenceScreen) findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
         mAdvancedReboot = findAndInitCheckboxPref(ADVANCED_REBOOT_KEY);
+	updateAdvancedRebootOptions(); 
 
         if (!android.os.Process.myUserHandle().equals(UserHandle.OWNER)) {
             disableForUser(mEnableAdb);
@@ -502,7 +503,7 @@ public class DevelopmentSettings extends PreferenceFragment
 
     private void resetAdvancedRebootOptions() {
         Settings.Secure.putInt(getActivity().getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT, 0);
+                Settings.Secure.ADVANCED_REBOOT, 1);
     }
 
     private void writeAdvancedRebootOptions() {
